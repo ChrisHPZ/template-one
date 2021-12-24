6174a734a6436679b2d537299afdf66e41e5521d
skyrim1974 <chrisai36@gmail.com>
2021-12-24 04:31:24 -0500

    updated styles and about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,20 @@
+import { RiReactjsFill } from "react-icons/ri";
+import { BsSuitHeartFill } from "react-icons/bs";
 const About = () => {
+	const innerContentScreenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 	return ( 
-		<h1>This is the about page</h1>
+		<div className="inner-content" id="about" style={{'height':innerContentScreenHeight - "93"}}>
+			<div className="inner-content-grid">
+				<div className="inner-content-grid-child" id="headpalm">					
+					<h1>Hi I'm Chris</h1>
+					<p>I'm a walking HR Violation that <BsSuitHeartFill style={{color:"red"}} />'s to code new things</p>
+				</div>
+				<div className="inner-content-grid-child">
+				<h2>Built with more <RiReactjsFill style={{color: "#80DEEA", position: "relative", top: "5px"}} />REACT JS <BsSuitHeartFill style={{color:"red"}} /></h2>
+					<p>I've been in the web development racket for 10+ years.</p>
+				</div>
+			</div>
+		</div>
 	 );
 }
  
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import { HiHome, HiInformationCircle } from "react-icons/hi";
 import { RiReactjsFill } from "react-icons/ri";
+import Headpalm  from "../img/headpalm.png";
 const Header = () => {
 	return ( 
 		<div className="header-container">
 			<div className="header-grid">
 				<div className="header-grid-child">
 					<RiReactjsFill style={{ width:"48px", height:"48px", color:"#80deea" }} />
+					<img src={Headpalm} alt="Chris" width="48" height="48" />
 				</div>
 				<div className="header-grid-child">
 					<ul>
