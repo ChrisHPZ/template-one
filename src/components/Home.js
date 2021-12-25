4e8326d7ceca75672788fbf2b041d83ac44fb369
skyrim1974 <chrisai36@gmail.com>
2021-12-25 08:41:21 -0500

    Clean up unneeded code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,16 @@
 import { BsSuitHeartFill } from "react-icons/bs";
 const Home = () => {
-	const innerContentScreenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 	return ( 
-		<div className="inner-content" id="home" style={{'height':innerContentScreenHeight - "93"}}>
-			<div className="inner-content-grid">
-				<div className="inner-content-grid-child">					
-					<h1>CSS Grid &amp; Flexbox</h1>
-					<p>Modern coding with less code. NO Page Builder BLAH!</p>
-				</div>
-				<div className="inner-content-grid-child">					
-					<h2>Built with Much REACT JS <BsSuitHeartFill style={{color: "red", position: "relative", top: "5px"}} /></h2>
-				</div>
+		<div className="inner-content" id="home">
+			<div className="inner-content-grid-child">					
+				<h2>CSS Grid &amp; Flexbox</h2>
+				<p>Modern coding with less code. NO Page Builder BLAH!</p>
+			</div>
+			<div className="inner-content-grid-child">					
+				<h2>Built with Much REACT JS <BsSuitHeartFill style={{color: "red", position: "relative", top: "5px"}} /></h2>
+			</div>
+			<div className="inner-content-grid-child">					
+				<h2>I Extend 2 columns <BsSuitHeartFill style={{color: "red", position: "relative", top: "5px"}} /></h2>
 			</div>
 		</div>
 	 );
