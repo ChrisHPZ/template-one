c3fb64970c16c907404da2842d90b799c26e3059
ChrisHPZ <chrisai36@gmail.com>
2022-01-22 09:40:48 -0500

    refactor responsive styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { RiReactjsFill } from "react-icons/ri";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdEmail } from "react-icons/md";
 import Headpalm  from "../img/headpalm.png";
+import { BsFacebook, BsTwitter, BsInstagram, BsYoutube } from "react-icons/bs";
 
 const Header = () => {
 	const navRef = useRef(null);
@@ -15,8 +16,12 @@ const Header = () => {
 		<div className="header-container">
 			<div className="header-grid">
 				<div className="header-grid-child">
-					<RiReactjsFill style={{ width:"48px", height:"48px", color:"#80deea" }} />
 					<img src={Headpalm} alt="Chris" width="48" height="48" />
+					<RiReactjsFill style={{ width:"48px", height:"48px", color:"#80deea" }} />
+					<BsFacebook style={{ width:"48px", height:"48px", color:"#80deea" }} />
+					<BsTwitter style={{ width:"48px", height:"48px", color:"#80deea" }} />
+					<BsInstagram style={{ width:"48px", height:"48px", color:"#80deea" }} />
+					<BsYoutube style={{ width:"48px", height:"48px", color:"#80deea" }} />					
 				</div>
 				<div className="header-grid-child">
 					<GiHamburgerMenu className="mobile-menu-trigger" onClick={onToggleClick} />
