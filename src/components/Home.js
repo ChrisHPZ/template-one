3822f14077b59bd7e6136a37cd9a56d6e62b8e8d
ChrisHPZ <chrisai36@gmail.com>
2022-01-22 06:49:44 -0500

    Added homepage widget section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,20 @@
 import { BsSuitHeartFill } from "react-icons/bs";
+import Widgets from "./Widgets";
 const Home = () => {
-	return ( 
-		<div className="inner-content" id="home">
-			<div className="inner-content-grid-child">					
-				<h2>CSS Grid &amp; Flexbox</h2>
-				<p>Modern coding with less code. NO Page Builder BLAH!</p>
-			</div>
-			<div className="inner-content-grid-child">					
-				<h2>Built with Much REACT JS <BsSuitHeartFill style={{color: "red", position: "relative", top: "5px"}} /></h2>
-			</div>
-			<div className="inner-content-grid-child">					
-				<h2>I Extend 2 columns <BsSuitHeartFill style={{color: "red", position: "relative", top: "5px"}} /></h2>
+	return (
+		<div className="home-container">
+			<div className="inner-content" id="home">
+				<div className="inner-content-grid-child">					
+					<h2>CSS Grid &amp; Flexbox</h2>
+					<p>Modern coding with less code. NO Page Builder BLAH!</p>
+				</div>
+				<div className="inner-content-grid-child">					
+					<h2>Built with Much REACT JS <BsSuitHeartFill style={{color: "red", position: "relative", top: "5px"}} /></h2>
+				</div>
+				<div className="inner-content-grid-child">					
+					<h2>I Extend 2 columns <BsSuitHeartFill style={{color: "red", position: "relative", top: "5px"}} /></h2>
+				</div>
+				<Widgets />
 			</div>
 		</div>
 	 );
---
+++ src/components/Widgets.js
@@ -0,0 +1,17 @@
+const Widgets = () => {
+    return ( 
+        <div className="widget-container">
+            <div className="circular">
+                Widget 1
+            </div>
+            <div className="circular">
+                Widget 2
+            </div>
+            <div className="circular">
+                Widget 3
+            </div>
+        </div>
+     );
+}
+ 
+export default Widgets;
